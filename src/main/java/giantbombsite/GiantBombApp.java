dc47ff867746a892f827d9eb34681f322b001301
John White <j.white@tr.com>
2018-04-24 13:19:47 -0500

    Initial commit - Basic Hello World Spring Boot app

---
+++ src/main/java/giantbombsite/GiantBombApp.java
@@ -0,0 +1,12 @@
+package giantbombsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiantBombApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GiantBombApp.class, args);
+  }
+}
